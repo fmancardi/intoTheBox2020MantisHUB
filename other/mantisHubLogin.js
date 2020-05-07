0532473c78268b355d103e7f5b26bb8eeb7b1141
Francisco Mancardi <francisco.mancardi@gmail.com>
2020-05-07 19:00:54 +0200

    important info added

--- other/mantisHubLogin.js
+++ other/mantisHubLogin.js
@@ -1,4 +1,12 @@
-const { openBrowser, goto, textBox, into, write, $, click, closeBrowser } = require('taiko');
+/* taiko --load mantisHubLogin.js */
+const { openBrowser, goto, textBox, into, write, $, 
+        click, closeBrowser } = require('taiko');
+
+/* CRITIC to be able to continue to interact
+   with browser from taiko console */        
+const { repl } = require('taiko/recorder');
+
+/* -------------------------------------- */
 (async () => {
     try {
         await openBrowser();
@@ -7,7 +15,14 @@ const { openBrowser, goto, textBox, into, write, $, click, closeBrowser } = requ
         await click($('#login-form > fieldset > input[type="submit"]'));
         await write("intothebox", into(textBox({id: "password"})));
         await click($('#login-form > fieldset > input[type="submit"]'));
+
+        /* Launch taiko REPL */
+        await repl();
+
     } catch (error) {
         console.error(error);
+    } finally {
+      console.log('Hi! You can use the browser');
+      console.log('When you will finish => CTRL-C to stop this script');
     }
 })();
